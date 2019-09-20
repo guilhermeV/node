2e15258ceec850e9c3247b984660e7ab21004dba
guilherme.pereira <guilherme.pereira@neolog.com.br>
2019-09-20 18:02:35 -0300

    Bla

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
@@ -19,15 +19,70 @@ var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 // error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error.pug');
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error.pug');
 });
+
+firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDsbNp1axrPdmTnqyFRqwhRoRZRZfw_uKk",
+    authDomain: "rosy-algebra-249618.firebaseapp.com",
+    databaseURL: "https://rosy-algebra-249618.firebaseio.com",
+    projectId: "rosy-algebra-249618",
+    storageBucket: "rosy-algebra-249618.appspot.com",
+    messagingSenderId: "319517874514",
+    appId: "1:319517874514:web:f47d1cc5b53e7ccb9bc28b"
+};
+
+// firebase.initializeApp({
+//     apiKey: '### FIREBASE API KEY ###',
+//     authDomain: '### FIREBASE AUTH DOMAIN ###',
+//     projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+// });
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// var defaultAuth = firebase.auth();
+var email = "guilherme.vinicius.a.p@gmail.com";
+var password = "testee";
+//
+// firebase.auth().signInWithEmailAndPassword(email, password).then(function (message) {
+// }).catch(function (error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     if (errorCode === 'auth/wrong-password') {
+//         console.log('Wrong password.');
+//     } else {
+//         console.log(errorMessage);
+//     }
+//     console.log(error);
+// });
+
+firebase.auth().signInWithEmailAndPassword(email, password).then(function (message) {
+    // res.send("logou mano");
+}).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode === 'auth/wrong-password') {
+        console.log('Wrong password.');
+    } else {
+        console.log(errorMessage);
+    }
+    console.log(error);
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,87 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+require("firebase/auth");
+require("firebase/app");
+require("firebase/firestore");
+
+var email = "guilherme.vinicius.a.p@gmail.com";
+var password = "testee";
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     let text = '';
-    fs.readFile('newfile.txt', 'utf8', function(err, data) {
+    fs.readFile('newfile.txt', 'utf8', function (err, data) {
         text = data;
         res.render('index', {title: 'Express', text: text});
     });
 });
+
+router.get('/login', function (req, res, next) {
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function (message) {
+        res.send("logou mano");
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+            res.send('Wrong password.');
+        } else {
+            res.send(errorMessage);
+        }
+        res.send(error);
+    });
+});
+
+router.get('/sql', function (req, res, next) {
+
+    var db = firebase.firestore();
+
+    db.collection("bike").get().then((querySnapshot) => {
+        console.log('success');
+        var docs = [];
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+            docs.push(doc.data());
+        });
+        res.send(docs);
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+});
+
+
+router.get('/insert', function (req, res, next) {
+    var db = firebase.firestore();
+    var data = {name: "nome5"};
+
+    db.collection('bike').doc('outraBike').set(data).then(function(success) {
+        console.log('suc');
+        res.send('suc');
+    }).catch(function(error) {
+        console.log(error);
+        res.send(error);
+    });
+
+});
+
+router.get('/insertAdd', function (req, res, next) {
+    var db = firebase.firestore();
+
+    db.collection("bike").add({
+        name: "nome3"
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            res.send('sucess');
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+            res.send('error');
+        });
+});
+
 
 router.post('/', function (req, res, next) {
     fs.writeFile('newfile.txt', req.body.text, function (err) {
